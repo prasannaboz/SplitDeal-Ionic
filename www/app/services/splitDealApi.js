841dc11488166031e7399f2514893d6bd110403b
prasanna <prasanna@blackorchidz.com>
2015-01-01 22:27:13 +0530

    $IonicLoad

--- www/app/services/splitDealApi.js
+++ www/app/services/splitDealApi.js
@@ -1,38 +1,43 @@
+//Register, Inject, Function Pattern
 (function () {
-
     'use strict';
 
-    angular.module('SplitDealApp').factory('splitDealApi', ['$http', '$q', splitDealApi]);
+    angular
+        .module('SplitDealApp')
+        .factory('splitDealApi', splitDealApi);
 
-    function splitDealApi($http, $q) {
+    splitDealApi.$inject = ['$http', '$q', '$ionicLoading'];
 
+    function splitDealApi($http, $q, $ionicLoading) {
         var currentItemId;
 
         //get list of items on watch list
         function getMyWatchList() {
             var deferred = $q.defer();
 
+            $ionicLoading.show({template: 'Loading...'});
+
             $http.get("http://api.everlive.com/v1/IMregDJC77R1b1yM/Items/")
                 .success(function (data) {
+                    console.log("watchlist data is called via http");
                     deferred.resolve(data);
-                    console.log(data);
+                    $ionicLoading.hide();
                 })
                 .error(function () {
                     console.log("Error via making http calls");
+                    $ionicLoading.hide();
                     deferred.reject();
                 });
             return deferred.promise;
-        };
+        }
 
         function setItemId(itemId) {
-
             currentItemId = itemId;
         }
 
         //get all users data
         function getUsersData() {
             var deferred = $q.defer();
-
             $http.get("http://api.everlive.com/v1/IMregDJC77R1b1yM/Users/")
                 .success(function (data) {
                     deferred.resolve(data);
@@ -43,13 +48,12 @@
                     deferred.reject();
                 });
             return deferred.promise;
-        };
+        }
 
         return{
             getMyWatchList: getMyWatchList,
             setItemId: setItemId,
             getUsersData: getUsersData
         };
-    };
-
+    }
 })();
