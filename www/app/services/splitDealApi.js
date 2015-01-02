580bf21843d4c8a316d0439ac7551aabeeb6b5f4
prasanna <prasanna@blackorchidz.com>
2015-01-02 13:07:11 +0530

    Offline usage and caching added

--- www/app/services/splitDealApi.js
+++ www/app/services/splitDealApi.js
@@ -6,28 +6,51 @@
         .module('SplitDealApp')
         .factory('splitDealApi', splitDealApi);
 
-    splitDealApi.$inject = ['$http', '$q', '$ionicLoading'];
+    splitDealApi.$inject = ['$http', '$q', '$ionicLoading', 'DSCacheFactory'];
 
-    function splitDealApi($http, $q, $ionicLoading) {
+    function splitDealApi($http, $q, $ionicLoading, DSCacheFactory) {
+
         var currentItemId;
+
+        self.myWatchListCache = DSCacheFactory.get("myWatchListCache");
+
+        self.myWatchListCache.setOptions({
+            onExpire: function (key, value) {
+                getMyWatchList().then(function () {
+                    console.log("my watch list cache was automatically refreshed");
+                }, function () {
+                    console.log("Error getting data, Putting expired items back into the cache",new Date());
+                    self.myWatchListCache.put(key, value);
+                });
+            }
+        });
 
         //get list of items on watch list
         function getMyWatchList() {
-            var deferred = $q.defer();
+            var deferred = $q.defer(),
+                cachekey = "watchlist",
+                myWatchlistData = self.myWatchListCache.get(cachekey);
 
-            $ionicLoading.show({template: 'Loading...'});
+            if (myWatchlistData) {
+                console.log("Found data inside cache", myWatchlistData);
+                deferred.resolve(myWatchlistData);
+            } else {
 
-            $http.get("http://api.everlive.com/v1/IMregDJC77R1b1yM/Items/")
-                .success(function (data) {
-                    console.log("watchlist data is called via http");
-                    deferred.resolve(data);
-                    $ionicLoading.hide();
-                })
-                .error(function () {
-                    console.log("Error via making http calls");
-                    $ionicLoading.hide();
-                    deferred.reject();
-                });
+                $ionicLoading.show({template: 'Loading...'});
+
+                $http.get("http://api.everlive.com/v1/IMregDJC77R1b1yM/Items/")
+                    .success(function (data) {
+                        console.log("watchlist data is called via http");
+                        self.myWatchListCache.put(cachekey, data);
+                        deferred.resolve(data);
+                        $ionicLoading.hide();
+                    })
+                    .error(function () {
+                        console.log("Error via making http calls");
+                        $ionicLoading.hide();
+                        deferred.reject();
+                    });
+            }
             return deferred.promise;
         }
 
